e5a51110336781feec8f588eb4d6d69fe9b013bb
Ro Han <105499295+rohan77700@users.noreply.github.com>
2023-07-24 23:37:43 +0530

    Admin login and signup

---
+++ admin_signup.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Signup</title>
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+	<style>
+		*{
+			padding: 0;
+			margin: 0;
+		}
+		body{
+			background: url("img/ccctimg.jpg");
+			align-items: center;
+			justify-content: center;
+			display: flex;
+		}
+		.center{
+			position: relative;
+			align-items: center;
+			margin-top: 60px; 
+			padding: 20px 70px 20px 70px;
+			width: 600px;
+			height: 770px;
+			opacity: 80%;
+			background: lightgray;
+			border-radius: 8px;
+		}
+		.logo{
+			text-align: center;
+		}
+		.logo img{
+			margin: auto 0 10px 0;
+			padding-top: 10px;
+			height: -400px;
+			width: 100px;
+		}
+		.label{
+			text-align: center;
+			padding: 30px 40px;
+			font-size: 20px;
+			font-weight: bold;
+		}
+		.login_form{
+			padding: 40px 150px;
+		}
+		.login_form .font{
+			font-size: 18px;
+			margin: 5px 0;
+		}
+		.login_form input{
+			height: 40px;
+			width: 350px;
+			padding: 0 5px;
+			font-size: 18px;
+			border: 1px solid silver;
+			border-radius: 10px;
+		}
+		.login_form input[type="submit"]{
+			margin: 45px 0 30px 0;
+			height: 45px;
+			width: 365px;
+			border-radius: 10px;
+			font-size: 20px;
+			color: white;
+			cursor: pointer;
+			font-weight: bold;
+			background: blue;
+			transition: .5s;
+		}
+		.login_form input[type="submit"]:hover{
+			background: lightgreen;
+		}
+		.login_form a{
+			padding-left: 50px;
+			font-size: 20px;
+			color: sandybrown;
+		}
+		.login_form a:hover{
+			color: saddlebrown;
+		}
+	</style>
+</head>
+<body>
+	<div class="center">
+		<div class="logo"><a href="#"><img src="img/CCCT.png" alt=""></a></div>
+		<h1 class="label">Centre For Computers Communication and Technology</h1><hr>
+			<form class="login_form" action="" method="post">
+				<div class="font">Designation</div>
+				<input type="text" name="user_designation" required>
+                <div class="font">Full Name</div>
+				<input type="text" name="fullname" required>
+				<div class="font">Username</div>
+				<input type="text" name="username" required>
+				<div class="font">Email</div>
+				<input type="email" name="email" required>
+				<div class="font">Password</div>
+				<input type="password" name="password" required>
+				<input type="submit" name="submit" value="Sign Up">
+				Already Signin : <a href="admin_login.php">Log In</a>
+			</form>
+
+		<?php
+			session_start();
+			if(isset($_POST['submit']))
+			{
+				$connection = mysqli_connect("localhost","root","");
+				$db = mysqli_select_db($connection,"sis");
+				$query = "insert into admin (fullname, user_designation, username, email, password) values ('$_POST[fullname]',
+                '$_POST[user_designation]','$_POST[username]','$_POST[email]','$_POST[password]')";
+				$query_run = mysqli_query($connection,$query);
+				if($query_run)
+				{
+					header("Location: admin_login.php");
+				}
+				else
+				{
+					echo "Error :".$query;
+				}
+					
+			}	
+						
+		?>
+	</div>
+</body>
+</html>
